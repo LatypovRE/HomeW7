4b93ae4dd244dba749ef8486079a5ddcc589a17d
Radel <latyp47@mail.ru>
2022-06-05 20:38:17 +0300

    Исправление

--- work1/Program.cs
+++ work1/Program.cs
@@ -11,7 +11,7 @@ for (int i = 0; i<3; i++)
 {
     for(int j = 0; j<4; j++)
     {
-        array[i,j] = random.NextDouble() * 100; 
+        array[i,j] = rand.NextDouble() * 100; 
         Console.Write("{0,4}", array[i, j]);
     }
     Console.WriteLine();
